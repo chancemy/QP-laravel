d3bf0fa89947ae6897b0fbf0aa5952c9621871ac
poiu60177 <82428714+poiu60177@users.noreply.github.com>
2021-07-08 17:42:15 +0800

    210708-購物車套件

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
@@ -174,6 +175,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        //購物車套件
+        Darryldecode\Cart\CartServiceProvider::class,
 
     ],
 
@@ -226,6 +229,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        //購物車
+        'Cart' => Darryldecode\Cart\Facades\CartFacade::class,
 
     ],
 
