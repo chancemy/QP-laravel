23bad7566df1a3b1f17d4b7befb1ad6101b15b66
chancemy <38711710+chancemy@users.noreply.github.com>
2021-07-09 13:51:10 +0800

    0709/model

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,31 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\News;
+use App\NewsType;
 use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
     public function news(){
-        return view('admin.news.item.index');
+        $news = News::with('news')->get();
+        return view('admin.news.item.index',compact('news'));
     }
 
     public function create(){
-        return view('admin.news.item.create');
+        $newsTypes = NewsType::get();
+        return view('admin.news.item.create',compact('newsTypes'));
     }
 
     public function store(Request $request){
-        dd($request->all());
+        News::create($request->all());
+        return redirect('/admin/news/item')->with('message','新增成功') ;
     }
 
-    public function edit(){
-
+    public function edit($id){
+        $oldNews  = News::find($id);
+        return view('admin.news.type.edit',compact('oldNews'));
     }
 
-    public function update(){
+    public function update(Request $request,$id){
 
+        $oldNews  = News::find($id);
+        $oldNews->type_id = $request->type_id;
+        $oldNews->title = $request->title;
+        $oldNews->img = $request->img;
+        $oldNews->date = $request->date;
+        $oldNews->description = $request->description;
+        $oldNews->remarks = $request->remarks;
+        $oldNews->is_display = $request->is_display;
+        $oldNews->save();
+        return redirect('/admin/news/item')->with('message','編輯成功') ;
     }
 
-    public function delete(){
-
+    public function delete($id){
+        $oldNews = News::find($id);
+        $oldNews->delete();
+        return redirect('/admin/news/item')->with('message','刪除成功') ;
     }
 }
--- app/Http/Controllers/NewsTypeController.php
+++ app/Http/Controllers/NewsTypeController.php
@@ -24,20 +24,20 @@ class NewsTypeController extends Controller
     }
 
     public function edit($id){
-        $oldNewsType  = newsType::find($id);
+        $oldNewsType  = NewsType::find($id);
         return view('admin.news.type.edit',compact('oldNewsType'));
     }
 
     public function update(Request $request,$id){
 
-        $oldNewsType  = newsType::find($id);
+        $oldNewsType  = NewsType::find($id);
         $oldNewsType->type_name = $request->type_name;
         $oldNewsType->save();
         return redirect('/admin/news/type')->with('message','編輯成功') ;
     }
 
     public function delete($id){
-        $oldNewsType = newsType::find($id);
+        $oldNewsType = NewsType::find($id);
         $oldNewsType->delete();
         return redirect('/admin/news/type')->with('message','刪除成功') ;
     }
