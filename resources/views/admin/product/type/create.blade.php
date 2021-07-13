75fa853aa11e0222fea20e0ab6056c14fed74a87
poiu60177 <82428714+poiu60177@users.noreply.github.com>
2021-07-13 11:19:09 +0800

    210713

--- resources/views/admin/product/type/create.blade.php
+++ resources/views/admin/product/type/create.blade.php
@@ -8,7 +8,7 @@
     <div class="container">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ asset('/home') }}">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{ asset('/admin') }}">後臺管理首頁</a></li>
                 <li class="breadcrumb-item"><a href="{{ asset('admin/product/type') }}">產品類別管理</a></li>
                 <li class="breadcrumb-item active" aria-current="page">新增產品類別</li>
             </ol>
